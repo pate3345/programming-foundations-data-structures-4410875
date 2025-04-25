ab956b8df9bb5812de47ea22e951c3e7d9bf1528
pate3345 <98113930+pate3345@users.noreply.github.com>
2025-04-25 17:52:11 +0000

    My Learning

--- 02/02_02/02_02b/main.py
+++ 02/02_02/02_02b/main.py
@@ -2,3 +2,22 @@
 Problem Statement: Compute the average number of pets each student 
 has in a given class. 
 '''
+
+#Simple array
+student_pet_count_list = [0,1,2,3,4,1,2,3,4,2,2,1,1]
+
+#Find the total student size.
+
+student_in_class = len(student_pet_count_list)
+
+# print(student_in_class)
+
+# sum = 24
+# avg = (sum/student_in_class)
+# print("avg is",avg)
+
+sum = 0
+for i in student_pet_count_list:
+    sum = sum + i
+print(sum)
+
--- 02/02_09/02_09b/main.py
+++ 02/02_09/02_09b/main.py
@@ -1,4 +1,18 @@
 def find_second_smallest(my_list):
-    return 0
+    result = (sorted(my_list))
+    answer = result[1]
+    return answer
 
-print(find_second_smallest([5, 8, 3, 2, 6]))
+def find_second_smallest_v2(my_list):
+    smallest = float('inf')
+    second_smallest = float('inf')
+    for num in my_list:
+        if num < smallest:
+            second_smallest = smallest
+            smallest = num
+        elif num < second_smallest:
+            second_smallest = num
+    return second_smallest
+
+
+print(find_second_smallest_v2([5, 8, 3, 2, 6]))
